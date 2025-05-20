33396967caa01dc4fa275a3ef1a6194ec226e40b
Dennis Olsen <dennan_lol@hotmail.com>
2025-05-20 10:49:25 +0200

    Tested in main

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,10 +1,112 @@
 package se.lexicon;
 
+import se.lexicon.DB.MySQLConnection;
+import se.lexicon.dao.CityDaoImpl;
+import se.lexicon.model.City;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Represents the entry point of the application.
  */
 public class Main {
     public static void main(String[] args) {
-        // TODO: Needs completion
+        //findById();
+        //findByCode();
+        //findByName();
+        //findAll();
+
+        //save();
+        //update();
+        //deleteById();
+    }
+
+    public static void findById(){
+        try{
+            CityDaoImpl cityDao = new CityDaoImpl(MySQLConnection.getConnection());
+
+            Optional<City> city = cityDao.findById(4082);
+
+            System.out.println(city.toString());
+            System.out.println("Operation is Done!");
+        } catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
+    }
+    public static void findByCode(){
+        try{
+            CityDaoImpl cityDao = new CityDaoImpl(MySQLConnection.getConnection());
+
+            List<City> cityList = cityDao.findByCode("SWE");
+
+            cityList.forEach(i -> System.out.println(i.toString()));
+            System.out.println("Operation is Done!");
+        } catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }    }
+    // LIMITED to the first 25
+    public static void findByName(){
+        try{
+            CityDaoImpl cityDao = new CityDaoImpl(MySQLConnection.getConnection());
+
+            List<City> cityList = cityDao.findByName("New Gothenburg2");
+
+            cityList.forEach(i -> System.out.println(i.toString()));
+            System.out.println("Operation is Done!");
+        } catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
+    }
+    public static void findAll(){
+        try{
+            CityDaoImpl cityDao = new CityDaoImpl(MySQLConnection.getConnection());
+
+            List<City> cityList = cityDao.findAll();
+
+            cityList.forEach(i -> System.out.println(i.toString()));
+            System.out.println("Operation is Done!");
+        } catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
+    }
+
+    public static void save(){
+        try{
+            CityDaoImpl cityDao = new CityDaoImpl(MySQLConnection.getConnection());
+
+            City savedCity = cityDao.save(new City("New Gothenburg2", "SWE", "testDistrict", 1));
+
+            System.out.println("savedItem = " + savedCity.toString());
+            System.out.println("Operation is Done!");
+
+        }catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
+    }
+    public static void update(){
+        try{
+            CityDaoImpl cityDao = new CityDaoImpl(MySQLConnection.getConnection());
+
+            cityDao.update(new City(4081,"New Gothenburg2", "SWE", "testDistrict", 2));
+
+            System.out.println("Update Operation is Done!");
+
+        }catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
+    }
+    public static void deleteById(){
+        try{
+            CityDaoImpl cityDao = new CityDaoImpl(MySQLConnection.getConnection());
+
+            cityDao.deleteById(4082);
+
+            System.out.println("Delete Operation is Done!");
+
+        }catch (SQLException e) {
+            System.out.println("MySQL DB Connection Failed.");
+        }
     }
 }
